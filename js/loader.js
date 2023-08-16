792c7a525a0a6d8476dccb6dc1a6b4d33b2e6fd1
zfrg <zfruiguo2010@163.com>
2023-08-16 09:55:07 +0800

    Site updated: 2023-08-16 09:55:06

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,34 @@
+function sleep(millisecond) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, millisecond)
+  })
+}
+var loader = document.getElementById("loader");
+var fade = (function () {
+  var timer;
+  var i = 0;
+  function change(tar) {
+    i++;
+    console.log(i);
+    console.log(loader.style.opacity);
+    var num = 1 - i / 100;
+	if (num != 0){
+      loader.style.opacity = num;
+	}else{
+	  loader.parentNode.removeChild(loader);
+	}
+    if (i === tar) {
+      clearTimeout(timer);
+      return false;
+    }
+    timer = setTimeout(function () {
+      change(tar);
+    }, 1);
+  }
+  return change;
+})();
 window.onload=function(){
-  var loader = document.getElementById('loader')
-  loader.parentNode.removeChild(loader);
+  fade(0);
 }
